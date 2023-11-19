5cc888e1a5d7d3d61138ced637c1bdf41934d992
priyatoshy <priyatoshsatpati1999@gmail.com>
2023-11-19 21:35:53 +0530

    fix

--- app.py
+++ app.py
@@ -8,22 +8,24 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 #CREATING APP AN app Object from flask class by giving the __name__ as parameter
 app = Flask(__name__)
-
-
-#setting up database orm with SQLAlchemy class
-
-#CREATING A SQLite3 Databased
 app.config['SQLALCHEMY_DATABASE_URI']="sqlite:///test.db"
 
-#CHANGING THE TRACK MODIFICATION(SIGNAL EMITING ETC)
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 
+#setting up database orm with SQLAlchemy class
+#CHANGING THE TRACK MODIFICATION(SIGNAL EMITING ETC)
+#CREATING A SQLite3 Databased
 #CREATING A DATABASE OBJECT FROM SQLALCHEMY CLASS WITH OUR app AS PARAMETER
+
+
 db=SQLAlchemy(app)
 
 
 #CREATING A TODOCLASS with db.model as a parameter(a sublclass within db)
-class Todo(db.Model):
+class Content(db.Model):
+   __tablename__="Content"
+   __table_args__ = {'extend_existing': True}
    #creating fields
    sno=db.Column(db.Integer,primary_key=True)
    title=db.Column(db.String(200),nullable=False)
@@ -39,20 +41,22 @@ class Todo(db.Model):
 
 #THE HOME PAGE AND ADDITION FUNCTION
 
+
+
 @app.route('/',methods=['GET','POST'])
 def home():
    if request.method =='POST':
       #print("POST")
       title1=request.form['title']
       desc1=request.form['desc']
-      todo=Todo(title=title1,desc=desc1)
+      todo=Content(title=title1,desc=desc1)
       db.session.add(todo)
       db.session.commit()
-      allTodo=Todo.query.all()
+      allTodo=Content.query.all()
       return render_template('index.html',allTodo=allTodo)
  
    else:
-      allTodo=Todo.query.all()
+      allTodo=Content.query.all()
       return render_template('index.html',allTodo=allTodo)
       
       
@@ -66,7 +70,7 @@ def home():
 #CREATING THE DELETE FUNCTION
 @app.route('/delete/<int:sno>')
 def delete(sno):
-   todo=Todo.query.filter_by(sno=sno).first()
+   todo=Content.query.filter_by(sno=sno).first()
    db.session.delete(todo)
    db.session.commit()
    #return "deleted"
@@ -80,14 +84,14 @@ def update(sno):
       
       title1=request.form['title']
       desc1=request.form['desc']
-      todo=Todo.query.filter_by(sno=sno).first()
+      todo=Content.query.filter_by(sno=sno).first()
       todo.title=title1
       todo.desc=desc1
       db.session.add(todo)
       db.session.commit()
       return redirect("/")
 
-   todo=Todo.query.filter_by(sno=sno).first()
+   todo=Content.query.filter_by(sno=sno).first()
    return render_template('update.html',todo=todo)
    
 
@@ -96,4 +100,5 @@ def update(sno):
 
 
 if __name__ == '__main__':
+   db.create_all()
    app.run(debug = True,port=8000)
